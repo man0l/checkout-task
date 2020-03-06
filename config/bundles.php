aab68b08579cfb9d17494ce1991ad94387117a03
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-06 23:02:14 +0200

    add nelmio alice for fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
 ];
