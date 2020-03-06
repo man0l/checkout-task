36f3c7113cd808a32d02a72f01213a7bd1a1db67
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-06 23:09:58 +0200

    add doctrine bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
